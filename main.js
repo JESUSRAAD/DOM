296619a9e82707203b41f5b4e7ad23998d305bd8
Jesus Raad <jesus.raad@students.gammatech.school>
2024-06-20 01:07:24 +0200

    listo all

--- main.js
+++ main.js
@@ -15,3 +15,11 @@ for (let i = 1; i < 101; i++) {
     
    }
 }
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const span= document.querySelectorAll("span")
+
+for (let i = 0; i < colors.length; i++) {
+    span[i].style.color=colors[i]
+    
+}
